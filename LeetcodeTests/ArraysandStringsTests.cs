6db1d6573babfcefdacfda8b62be9ffd9b8d1d1d
DanielMadaghjian <danielmadaghjian@gmail.com>
2024-05-11 18:17:23 +0100

    first commit

---
+++ LeetcodeTests/ArraysandStringsTests.cs
@@ -0,0 +1,22 @@
+using leetcode_answers;
+
+namespace LeetcodeTests
+{
+    public class ArraysAndStringsTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            //int[] nums = [3, 2, 2, 3];
+            //int val = 3;
+            //int result = Leetcode_150_Questions.Program.RemoveElement(nums, val);
+            //int correctResult = 2;
+            //Assert.That(result, Is.EqualTo(correctResult));
+        }
+    }
+}
---
+++ leetcode_answers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_answers
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // arrays and strings
+            ArraysandStringsAnswers arraysandStringsAnswers = new ArraysandStringsAnswers();
+
+        }
+    }
+}
